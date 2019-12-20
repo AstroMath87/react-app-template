5e014400a66fa39d5914d0c06afa6367130ffc55
AstroMath87 <conklinradio@gmail.com>
2019-12-20 14:35:25 -0700

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'; 
+import * as d3 from 'd3'; 
+
+class App extends Component {
+
+
+render() { 
+
+    return(
+
+        <div>
+            <h1> 
+                Hello!
+            </h1>
+        </div>
+    )
+}
+    
+}
+
+export default App;
+ 
